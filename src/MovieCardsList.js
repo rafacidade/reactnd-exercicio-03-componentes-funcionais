6bca28f481d09b204b4d5ef108dd1ef91db36f97
root <rafal_cidade@hotmail.com>
2018-11-25 14:49:34 +0000

    First commit

---
+++ src/MovieCardsList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import MovieCard from './MovieCard';
+
+function MovieCardsList(props) {
+  	const usersByMovie = {}
+    
+    props.profiles.forEach(profile => {
+      const movieID = profile.favoriteMovieID;
+
+      if (usersByMovie[movieID]) {
+        usersByMovie[movieID].push(profile.userID);
+      } else {
+        usersByMovie[movieID] = [profile.userID];
+      }
+    });
+
+    const movieCards = Object.keys(props.movies).map(id => (
+      <MovieCard
+        key={id}
+        users={props.users}
+        usersWhoLikedMovie={usersByMovie[id]}
+        movieInfo={props.movies[id]}
+      />
+    ));
+
+    return <ul>{movieCards}</ul>;  
+}
+
+export default MovieCardsList;
